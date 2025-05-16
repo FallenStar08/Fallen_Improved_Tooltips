20ef71bef3d1f280f5385318ded577d027f8cb7a
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2025-05-16 22:03:25 +0200

    version bump

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using Fallen_LE_Mods;
 using MelonLoader;
 
-[assembly: MelonInfo(typeof(MyMod), "FallenStar's Improved Tooltips", "2.0.1", "FallenStar")]
+[assembly: MelonInfo(typeof(MyMod), "FallenStar's Improved Tooltips", "2.0.2", "FallenStar")]
 [assembly: MelonGame("Eleventh Hour Games", "Last Epoch")]
